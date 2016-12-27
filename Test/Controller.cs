feb2eb633d3436aa1683174a8032eb04d8971e21
Rick <Rick@DESKTOP-6BB2TGB>
2016-12-27 20:04:27 +0100

    Projektdateien hinzufügen.

---
+++ Test/Controller.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Controller
+    {
+        private ConsoleKeyInfo key;
+        bool Running= true;
+        int posx= 5, posy= 5;
+    
+        public void InputLoop()
+        {
+            Spielfeld s1 = new Spielfeld();
+            while (Running)
+            {
+                key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if(posx==0)
+                        {
+                            Console.WriteLine("dont do that");
+                        }
+                        else
+                        {
+                            s1.set(posy, posx, 0);
+                            s1.set(posy, --posx, 1);
+                            Console.Clear();
+                            s1.DrawField();
+                        }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (posx == 9)
+                        {
+                            Console.WriteLine("dont do that");
+                        }
+                        else
+                        {
+                            s1.set(posy, posx, 0);
+                            s1.set(posy, ++posx, 1);
+                            Console.Clear();
+                            s1.DrawField();
+                        }
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        if (posy == 0)
+                        {
+                            Console.WriteLine("dont do that");
+                        }
+                        else
+                        {
+                            s1.set(posy, posx, 0);
+                            s1.set(--posy, posx, 1);
+                            Console.Clear();
+                            s1.DrawField();
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if(posy==9)
+                        {
+                            Console.WriteLine("dont do that");
+                        }
+                        else
+                        {
+                            s1.set(posy, posx, 0);
+                            s1.set(++posy, posx, 1);
+                            Console.Clear();
+                            s1.DrawField();
+                        }
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Test/Spielfeld.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Spielfeld
+    {
+        int[,] spielfeld = new int[10,10];
+        public void set(int y, int x,int z)
+        {
+            this.spielfeld[y, x] = z;
+        }
+        public int get(int y, int x)
+        {
+            return this.spielfeld[y,x];
+        }
+
+        public void DrawField()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(spielfeld[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
